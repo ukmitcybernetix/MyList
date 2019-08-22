be5bbcf98c3bef5122b054c57d766f9d707e14f4
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-22 22:34:08 +0700

    Update DatabaseConstract Class

--- app/src/main/java/com/zendev/mylist/database/DatabaseContract.java
+++ app/src/main/java/com/zendev/mylist/database/DatabaseContract.java
@@ -1,4 +1,16 @@
 package com.zendev.mylist.database;
 
+import android.provider.BaseColumns;
+
 public class DatabaseContract {
+
+    static String TABLE_LIST = "list";
+
+    static final class ListColumns implements BaseColumns {
+
+        static String TITLE = "title";
+        static String DESCRIPTION = "description";
+        static String DATE = "date";
+
+    }
 }
