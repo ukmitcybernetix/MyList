526106a24225231f5ec0658a67409551d0a3ce1c
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-22 22:19:40 +0700

    Add Getter & Setter, Parcelable to The List Class

--- app/src/main/java/com/zendev/mylist/model/List.java
+++ app/src/main/java/com/zendev/mylist/model/List.java
@@ -1,4 +1,79 @@
 package com.zendev.mylist.model;
 
-public class List {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class List implements Parcelable {
+
+    private int id;
+    private String title;
+    private String description;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.title);
+        dest.writeString(this.description);
+        dest.writeString(this.date);
+    }
+
+    public List() {
+    }
+
+    protected List(Parcel in) {
+        this.id = in.readInt();
+        this.title = in.readString();
+        this.description = in.readString();
+        this.date = in.readString();
+    }
+
+    public static final Parcelable.Creator<List> CREATOR = new Parcelable.Creator<List>() {
+        @Override
+        public List createFromParcel(Parcel source) {
+            return new List(source);
+        }
+
+        @Override
+        public List[] newArray(int size) {
+            return new List[size];
+        }
+    };
 }
