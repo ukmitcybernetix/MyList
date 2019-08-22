19a5da4fb7d542558b52a041641c834714edd207
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-22 22:55:20 +0700

    Update ListHelper Class

--- app/src/main/java/com/zendev/mylist/database/ListHelper.java
+++ app/src/main/java/com/zendev/mylist/database/ListHelper.java
@@ -1,4 +1,31 @@
 package com.zendev.mylist.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static com.zendev.mylist.database.DatabaseContract.TABLE_LIST;
+
 public class ListHelper {
+
+    private static final String DATABASE_TABLE = TABLE_LIST;
+    private static DatabaseHelper databaseHelper;
+    private static ListHelper INSTANCE;
+
+    private static SQLiteDatabase database;
+
+    public ListHelper(Context context) {
+        databaseHelper = new DatabaseHelper(context);
+    }
+
+    public static ListHelper getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (SQLiteOpenHelper.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new ListHelper(context);
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
