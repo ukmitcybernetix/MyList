30bed11c2e33697dd4c44a86c0f0f325d831e34b
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-22 22:40:11 +0700

    Update DatabasseHelper Class

--- app/src/main/java/com/zendev/mylist/database/DatabaseHelper.java
+++ app/src/main/java/com/zendev/mylist/database/DatabaseHelper.java
@@ -1,4 +1,20 @@
 package com.zendev.mylist.database;
 
 public class DatabaseHelper {
+
+    public static String DATABASE_NAME = "dbmylist";
+
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String SQL_CREATE_TABLE_LIST = String.format("CREATE TABLE %s"
+                    + " (%s INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL," +
+                    " %s TEXT NOT NULL)",
+            DatabaseContract.TABLE_LIST,
+            DatabaseContract.ListColumns._ID,
+            DatabaseContract.ListColumns.TITLE,
+            DatabaseContract.ListColumns.DESCRIPTION,
+            DatabaseContract.ListColumns.DATE
+    );
 }
