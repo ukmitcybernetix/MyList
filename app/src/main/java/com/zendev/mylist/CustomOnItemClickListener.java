acbd74221abdb7c9b5e10462fff14bfbd75bae87
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-23 09:38:10 +0700

    Update public interface OnItemClickCallback {

--- app/src/main/java/com/zendev/mylist/CustomOnItemClickListener.java
+++ app/src/main/java/com/zendev/mylist/CustomOnItemClickListener.java
@@ -1,4 +1,23 @@
 package com.zendev.mylist;
 
-public class CustomOnItemClickListener {
+import android.view.View;
+
+public class CustomOnItemClickListener implements View.OnClickListener {
+
+    private int position;
+    private OnItemClickCallback onItemClickCallback;
+
+    public CustomOnItemClickListener(int position, OnItemClickCallback onItemClickCallback) {
+        this.position = position;
+        this.onItemClickCallback = onItemClickCallback;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    public interface OnItemClickCallback {
+        void onItemClicked(View view, int position);
+    }
 }
