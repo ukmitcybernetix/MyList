f6d6e645713fe81575dbb09c1bc8c25be1ddad45
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-08-23 11:11:17 +0700

    Update LoadNotesCallback Listener

--- app/src/main/java/com/zendev/mylist/LoadNotesCallback.java
+++ app/src/main/java/com/zendev/mylist/LoadNotesCallback.java
@@ -1,4 +1,11 @@
 package com.zendev.mylist;
 
+import com.zendev.mylist.model.List;
+
+import java.util.ArrayList;
+
 public interface LoadNotesCallback {
+    void preExecute();
+
+    void postExecute(ArrayList<List> lists);
 }
